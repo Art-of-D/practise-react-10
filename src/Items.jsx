b03d5c8e9d24426df33f6462614948bbfb13fa88
Artur Didur <didur.art@gmail.com>
2025-08-25 23:46:54 +0200

    l start

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,7 +1,20 @@
-import SingleItem from './SingleItem';
+import { use } from "react";
+import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import api from "./utils";
+
 const Items = ({ items }) => {
+  const response = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const res = await api.get("/tasks");
+      return res.data;
+    },
+  });
+
+  console.log(response);
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
